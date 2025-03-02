bea3d603210540e051f8c20d8cf3f4ada5ce319d
VoxScripts <163832436+VoxScripts@users.noreply.github.com>
2025-03-02 23:02:23 +0400

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,26 @@
 
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+// Configure CORS properly
+app.use(cors({
+  origin: "https://dicerollerui.z1.web.core.windows.net"
+  METHOD: ["GET", "POST", "UPDATE"]
+}));
+
+// Existing endpoints
+app.get('/api/roll/:sides', (req, res) => {
+  const sides = parseInt(req.params.sides);
+  const result = Math.floor(Math.random() * sides) + 1;
+  res.json({ sides, result });
+});
+
+app.get('/wakeup', (req, res) => {
+  res.send('Server is awake');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log('Server running');
+});
